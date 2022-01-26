a497af46c351f31a461ffe84f470e9c3f1af6aea
byteErudite <raghuvanshivibhu@gmail.com>
2022-01-27 00:40:42 +0530

    Added implementation for search by name api

--- Main/main.go
+++ Main/main.go
@@ -4,11 +4,13 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Superhero struct {
+	Id          int    `json:"id"`
 	Name        string `json:"name"`
 	Superpower  string `json:"superpower"`
 	Nationality string `json:"nationality"`
@@ -31,18 +33,33 @@ func deleteSuperHeroes(w http.ResponseWriter, r *http.Request) {
 }
 
 func getSuperHeroeByName(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, hero := range superheroes {
+		if strconv.Itoa(hero.Id) == params["id"] {
+			json.NewEncoder(w).Encode(hero)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Superhero{})
+}
 
+func greet(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode("Welcome to the superhero page!!")
 }
 
 func main() {
-	superheroes = append(superheroes, Superhero{"Ironman", "Ironsuit", "USA", 45})
-	superheroes = append(superheroes, Superhero{"Hulk", "super-strength", "USA", 43})
+	superheroes = append(superheroes, Superhero{1, "Ironman", "Ironsuit", "USA", 45})
+	superheroes = append(superheroes, Superhero{2, "Hulk", "super-strength", "USA", 43})
 
 	r := mux.NewRouter()
+	r.HandleFunc("/superhero/greet", greet).Methods("GET")
 	r.HandleFunc("/superhero/all", getAllSuperHeroes).Methods("GET")
-	r.HandleFunc("/superhero/all", getSuperHeroeByName).Methods("GET")
+
+	r.HandleFunc("/superhero/{id}", getSuperHeroeByName).Methods("GET")
 	r.HandleFunc("/superhero/create", createSuperHeroes).Methods("POST")
-	r.HandleFunc("/superhero/delete/{name}", deleteSuperHeroes).Methods("DELETE")
+	r.HandleFunc("/superhero/delete/{id}", deleteSuperHeroes).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 
